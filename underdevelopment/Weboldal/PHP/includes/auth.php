76b32474751f0ff07b9c2b94a621ce97f17d71f4
xx0422 <vargariky222@gmail.com>
2026-03-14 13:55:36 +0100

    update

--- underdevelopment/Weboldal/PHP/includes/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-
-
--- underdevelopment/Weboldal/PHP/public/api/debug_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-echo json_encode([
-    "session_id" => session_id(),
-    "session" => $_SESSION
-]);
